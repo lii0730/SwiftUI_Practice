806f64eeae887968906ae722ff580a4b4ba781bc
LeeHsss <lii0730@icloud.com>
2021-07-26 19:57:15 +0900

    Initial Commit

---
+++ SwiftUI_Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI_Practice
+//
+//  Created by LeeHsss on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
